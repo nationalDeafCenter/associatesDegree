95d00db23d1a4e5b456de767da433f0dd0685707
adamSales <asales@utexas.edu>
2018-10-19 18:12:18 -0500

    end of day october 19 (I hope)

--- attainment12-16.r
+++ attainment12-16.r
@@ -25,6 +25,8 @@ source('generalCode/median.r')
 
 ## load in dataset
 source('make5yrDat.r')
+save(sdat,file='fiveYearDat.RData')
+
 
 sdat <- mutate(sdat, young=agep<46)
 
@@ -82,12 +84,109 @@ write.xlsx(list("Age 25-64"=attain(attain25.64),"Age 25-45"=attain(attain25.45))
 print(nrow(deafAssP <- filter(deaf,cc)))
 print(nrow(deafAss <- filter(deaf,attain=='Associates degree')))
 print(nrow(hearAssP <- filter(hear,cc)))
-print(nrow(hearAssP <- filter(hear,attain=='Associates degree')))
+print(nrow(hearAss <- filter(hear,attain=='Associates degree')))
 
 rm(deaf,hear); gc()
 
 ## for sex, race/eth, disability type, field of degree
 ## estimate percent, med. income, employment
 
+propEst <- function(subsets,sdat,groupNames){
+    out <- NULL
+    for(subst in subsets)
+        out <- rbind(out,estSEstr(subst,sdat=sdat)*c(100,100,1))
+    out <- as.data.frame(out)
+    names(out) <- c('Percent','SE','Sample Size')
+    out <- cbind(subgroup=if(missing(groupNames)) subsets else groupNames,out)
+    out
+}
+
+moneyEst <- function(subsets,sdat,groupNames){
+    if(missing(groupNames)) groupNames <- subsets
+    earn <- svby('earn',subsets=subsets,FUN=medStr,sdat=sdat)
+    colnames(earn) <- c('Subgroup Percent','Median Earnings','Earnings SE','n')
+    inc <- svby('inc',subsets=subsets,FUN=medStr,sdat=sdat,prop=FALSE)
+    colnames(inc) <- c('Median Income','Income SE','n')
+    out <- as.data.frame(cbind(earn[,-ncol(earn)],inc))
+    out <- cbind(Subgroup=groupNames,out)
+    rownames(out) <- NULL
+    out
+}
+
+employEst <- function(subsets,sdat){
+    groupNames <- if(!is.null(names(subsets))) names(subsets) else subsets
+
+    ocs <- c('employed','unemployed','esr==6','fulltime')
+
+    out <- sapply(ocs,function(oo) svby(oo,subsets=subsets,FUN=estSEstr,sdat=sdat),simplify=FALSE)
+
+    p <- out[[1]][,1]
+    n <- out[[1]][,ncol(out[[1]])]
+    out <- sapply(out,function(x) x[,-c(1,ncol(x))],simplify=FALSE)
+    out <- do.call('cbind',out)*100
+    colnames(out) <- outer(c('Percent','SE'),Hmisc::capitalize(gsub('esr==6','unemployed',ocs)),paste)
+    out <- as.data.frame(out)
+    out <- cbind(Subgroup=groupNames,`Subgroup Percent`=p,out,`Sample Size`=n)
+    rownames(out) <- NULL
+
+    out
+}
 
 
+eachOut <- function(domain,subsets,outcome){
+    fun <- switch(outcome,'Subgroup Proportions'=propEst,'Income'=moneyEst,'Employment'=employEst)
+    dfend <- switch(domain,"Associates Degree"="Ass","Associates Degree Or Higher"="AssP")
+    dd <- fun(subsets,get(paste0('deaf',dfend),envir=.GlobalEnv))
+    hh <- fun(subsets,get(paste0('hear',dfend),envir=.GlobalEnv))
+
+    dd[,sapply(dd,is.numeric)] <- round(dd[,sapply(dd,is.numeric)],1)
+    hh[,sapply(hh,is.numeric)] <- round(hh[,sapply(hh,is.numeric)],1)
+
+    rbind(
+        c('','Deaf',rep('',ncol(dd)-2),'Hearing',rep('',ncol(hh)-2)),
+        c(names(dd),names(hh)[-1]),
+        cbind(as.matrix(dd),as.matrix(hh)[,-1]))
+}
+
+raceEthSubs <- paste('raceEth==',levels(deafAss$raceEth))
+names(raceEthSubs) <- levels(deafAss$raceEth)
+
+fodSubs <- paste('fod==',levels(deafAss$fod))
+names(fodSubs) <- levels(deafAss$fod)
+fodSubs <- c(fodSubs,'STEM Field'='sciengp==1','STEM-Related Field'='sciengrlp==1')
+
+for(domain in c('Associates Degree','Associates Degree Or Higher')){
+    for(outcome in c('Subgroup Proportions','Income','Employment')){
+        out <- list(
+            "By Sex"=eachOut(domain=domain,subsets=c(Male="sex==1",Female="sex==2"),outcome=outcome),
+            "By Race/Ethnicity"=eachOut(domain=domain,subsets=raceEthSubs,outcome=outcome),
+            "By Disability Type"=eachOut(domain=domain,
+                                         subsets=c('No Other Disability'="diss==0",
+                                                   'Any Other Disability'="diss==1",
+                                                   'Blind'='blind==1'),outcome=outcome),
+            "By Field of Degree"=eachOut(domain=domain,fodSubs,outcome=outcome))
+        write.xlsx(out,paste0(domain,'-',outcome,'Ages 25-64'),colNames=FALSE,rowNames=FALSE,
+                   colWidths=rep('auto',length(out)))
+    }
+}
+
+
+deafAss <- filter(deafAss,young)
+deafAssP <- filter(deafAssP,young)
+hearAss <- filter(hearAss,young)
+hearAss <- filter(hearAssP,young)
+
+for(domain in c('Associates Degree','Associates Degree Or Higher')){
+    for(outcome in c('Subgroup Proportions','Income','Employment')){
+        out <- list(
+            "By Sex"=eachOut(domain=domain,subsets=c(Male="sex==1",Female="sex==2"),outcome=outcome),
+            "By Race/Ethnicity"=eachOut(domain=domain,subsets=raceEthSubs,outcome=outcome),
+            "By Disability Type"=eachOut(domain=domain,
+                                         subsets=c('No Other Disability'="diss==0",
+                                                   'Any Other Disability'="diss==1",
+                                                   'Blind'='blind==1'),outcome=outcome),
+            "By Field of Degree"=eachOut(domain=domain,fodSubs,outcome=outcome))
+        write.xlsx(out,paste0(domain,'-',outcome,'Ages 25-45'),colNames=FALSE,rowNames=FALSE,
+                   colWidths=rep('auto',length(out)))
+    }
+}
